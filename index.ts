359e1b6d27790bb3e9791f31a9253bea51cc5617
Glitch (silver-pretty-toad) <none>
2022-08-17 14:20:07 +0000

    🟨🚸 Updated with Glitch

---
+++ index.ts
@@ -0,0 +1,6 @@
+var idle = 0
+const hello = 1 // Set to three.
+
+if (hello > 2) {
+  idle -= 1
+}
---
+++ script.js
@@ -0,0 +1,53 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+console.log("Hello, world!");
+
+console.log(
+  `%c<h1 %cstyle=%c"color:yellow;"%c>%cHello World!%c</h1>`,
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:turquoise;",
+  "font-size:17px;color:darkgreen;",
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:rgb(0, 17, 0);",
+  "font-size:17px;color:orange;",
+)
+
+console.log(
+  `%c<ol>%c<li>%cHello!%c</li>%c<li %cstyle=%c"background:rgba(128,128,128,0.5);"%c>%cBackground Half a Full Transpaent!%c</li>%c</ol>`,
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:rgb(0, 17, 0);",
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:turquoise;",
+  "font-size:17px;color:darkgreen;",
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:rgb(0, 17, 0);",
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:orange;",
+)
+
+console.log(
+   `%ch1 %c{
+ %ccolor: %ctomato%c;
+ %ctext-shadow: %c1px 2px %cgreen%c;
+%c}`,
+  "font-size:17px;color:orange;",
+  "font-size:17px;color:darkmagenta;",
+  "font-size:17px;color:yellow;",
+  "font-size:17px;color:hotpink;",
+  "font-size:17px;color:darkgray;",
+  "font-size:17px;color:yellow;",
+  "font-size:17px;color:blue;",
+  "font-size:17px;color:hotpink;",
+  "font-size:17px;color:darkgray;",
+  "font-size:17px;color:darkmagenta;",
+)
+
+console.log(
+ `%cSCORE:0
+%cHIGHSCORE:0`,
+  "font-size:25px;",
+  "font-size:25px;",
+)
